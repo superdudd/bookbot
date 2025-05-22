c767c6ae16570e74fad3eea6e180eeacfdc611a3
superdudd <superdudd@gmail.com>
2025-05-22 16:23:30 -0400

    finalized char count

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ def get_book_text(file_path):
     with open(file_path) as f:
         book_text = f.read()
     return (book_text)
+from stats import get_char_count
 from stats import get_book_count
 def main():
     try:
         book_text = get_book_text("books/frankenstein.txt")
         book_count = get_book_count(book_text)
+        char_count = get_char_count(book_text)
         print(f"{book_count} words found in the document")
+        for char, count in sorted(char_count.items(), key=lambda item: item[1], reverse=True):
+            print(f"'{char}': {count}")
     except FileNotFoundError:
         print("Error: File not found. Please check the path and try again.")
         return
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_book_count(book_text):
     #"""Counts the total number of words in a text file."""
     return len(book_text.split()) 
+def get_char_count(book_text):
+    #"""Counts the number of times a character appears in a text file."""
+    # Example: Count the number of times 'a' appears
+    char_count = {}
+    for char in book_text:
+        char_lower =str.lower(char)
+        if char_lower in char_count:
+            char_count[char_lower] += 1
+        else:
+            char_count[char_lower] = 1
+    return char_count
